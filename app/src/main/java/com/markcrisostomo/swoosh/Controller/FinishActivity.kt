dd6a5e16a5c967f7d0d0c4cc7fd37381dba3d13f
Mark Crisostomo <mark@deviseinteractive.com>
2017-12-15 17:45:45 -0800

    refactored to parcelables

--- app/src/main/java/com/markcrisostomo/swoosh/Controller/FinishActivity.kt
+++ app/src/main/java/com/markcrisostomo/swoosh/Controller/FinishActivity.kt
@@ -2,9 +2,9 @@ package com.markcrisostomo.swoosh.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.markcrisostomo.swoosh.Model.Player
 import com.markcrisostomo.swoosh.R
-import com.markcrisostomo.swoosh.Utilities.EXTRA_LEAGUE
-import com.markcrisostomo.swoosh.Utilities.EXTRA_SKILL
+import com.markcrisostomo.swoosh.Utilities.EXTRA_PLAYER
 import kotlinx.android.synthetic.main.activity_finish.*
 
 class FinishActivity : AppCompatActivity() {
@@ -13,9 +13,7 @@ class FinishActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_finish)
 
-        val league = intent.getStringExtra(EXTRA_LEAGUE)
-        val skill = intent.getStringExtra(EXTRA_SKILL)
-
-        searchLeaguesText.text = "Looking for $league $skill league near you..."
+        val player = intent.getParcelableExtra<Player>(EXTRA_PLAYER)
+        searchLeaguesText.text = "Looking for ${player.league} ${player.skill} league near you..."
     }
 }
